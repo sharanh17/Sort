ed6d975e70bf313bb539c6255247348c7f650fe6
sharanh17 <sharanhidaduggi17@gmail.com>
2024-01-19 15:14:26 +0530

    first

---
+++ src/XTable.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+
+const XTable = () => {
+  const initialData = [
+    { date: "2022-09-01", views: 100, article: "Article 1" },
+    { date: "2023-09-01", views: 100, article: "Article 1" },
+    { date: "2023-09-02", views: 150, article: "Article 2" },
+    { date: "2023-09-02", views: 120, article: "Article 3" },
+    { date: "2020-09-03", views: 200, article: "Article 4" },
+  ];
+
+  const [data, setData] = useState(initialData);
+  const [sortBy, setSortBy] = useState(null);
+
+  const handleSortByDate = () => {
+    const sortedData = [...data].sort((a, b) => {
+      if (a.date === b.date) {
+        return b.views - a.views;
+      }
+      return new Date(b.date) - new Date(a.date);
+    });
+
+    setData(sortedData);
+    setSortBy("date");
+  };
+
+  const handleSortByViews = () => {
+    const sortedData = [...data].sort((a, b) => {
+      if (a.views === b.views) {
+        return new Date(b.date) - new Date(a.date);
+      }
+      return b.views - a.views;
+    });
+
+    setData(sortedData);
+    setSortBy("views");
+  };
+
+  return (
+    <div>
+      <h1>Date and Views Table</h1>
+      <button onClick={handleSortByDate}>Sort by Date</button>
+      <button onClick={handleSortByViews}>Sort by Views</button>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Views</th>
+            <th>Article</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td>{item.date}</td>
+              <td>{item.views}</td>
+              <td>{item.article}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default XTable;
+
